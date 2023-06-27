78e4f487828faa4679c27081ca16018aa6b97d6a
TrashCoder94 <jmottershead1994@gmail.com>
2023-06-27 18:28:05 +0100

    Update: Lib
    :penguin: - Removing lib files (these were 0kb anyway so I think that is part of the problem I'm seeing here...). Build scripts will now be responsible for sorting out SFML library for Linux

--- Scripts/BuildMachine/BuildMachine-Linux-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build-Debug.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install xorg-dev libxrandr-dev libxcursor-dev libudev-dev libopenal-dev libflac-dev libvorbis-dev libgl1-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev xvfb fluxbox
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ThirdParty/SFML-Binaries-Test/Linux/lib
-
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
