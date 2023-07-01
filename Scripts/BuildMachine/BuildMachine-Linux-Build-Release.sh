9b9eebe53cae65012b32cca264358bcc57403176
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-01 20:44:19 +0100

    Update: Scripts
    :penguin: - Making sure Release compiles too

--- Scripts/BuildMachine/BuildMachine-Linux-Build-Release.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build-Release.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install xorg-dev libxrandr-dev libxcursor-dev libudev-dev libopenal-dev libflac-dev libvorbis-dev libgl1-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev xvfb fluxbox
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ThirdParty/SFML-Binaries-Test/Linux/lib
-
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
 if [ $? -ne 0 ] 
 then
 echo "Generating project files failed, exiting out..."
-find . -name *.so
 exit 1
 fi
 
@@ -18,6 +13,5 @@ make config=release_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building release configuration failed, exiting out..."
-find . -name *.so
 exit 1
 fi
