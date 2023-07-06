9bfda6a3aff5e747343f2268398b40913ee24668
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-06 21:25:39 +0100

    Update premake5.lua
    :apple: - MISSING SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- premake5.lua
+++ premake5.lua
@@ -131,7 +131,7 @@ function includeAndLinkSFML()
 		libdirs
 		{
 			"/usr/local/Cellar/sfml/2.5.1_2/lib",
-			"/usr/local/Cellar/sfml/2.5.1_2/extlib"
+			"/usr/local/Cellar/sfml/2.5.1_2/extlibs"
 		}
 	
 		links
