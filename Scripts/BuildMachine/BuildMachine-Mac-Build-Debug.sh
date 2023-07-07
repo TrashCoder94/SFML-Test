30d54dd01c878fcca1c0425c7bb15c2995b6a266
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-07 15:47:22 +0100

    Update BuildMachine-Mac-Build-Debug.sh
    :apple: - Maybe the weird tilde is messing up the file path for find?

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -8,7 +8,7 @@ make config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
-find ~/usr/local/Cellar/sfml/ -type f
+find /usr/local/Cellar/sfml/ -type f
 realpath Window.hpp
 realpath FLAC.framework
 realpath sfml-audio.framework
