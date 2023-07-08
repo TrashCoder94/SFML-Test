a6cae27931942ce4ee33e44b578e93bc8bffffda
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-08 16:59:53 +0100

    Update BuildMachine-Mac-Build-Debug.sh
    :apple: - This sucks part 12

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "Copying SFML frameworks to /usr/local/lib folder"
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /usr/local/lib
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /usr/local/lib
+cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /Library/Frameworks
+cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /Library/Frameworks
 
 echo "Library path environment variable before:"
 echo $LD_LIBRARY_PATH
 
 echo "Setting Library path environment variable"
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/Library/Frameworks:$LD_LIBRARY_PATH
 
 echo "Library path environment variable after:"
 echo $LD_LIBRARY_PATH
@@ -21,6 +21,6 @@ make --debug=v config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
-ll /Library/Frameworks/
+realpath sfml-audio
 exit 1
 fi
