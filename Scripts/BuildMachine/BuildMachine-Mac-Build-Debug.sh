90610ba79e8a3930dcbfa5f02265953ea03c01d6
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-08 20:29:10 +0100

    Update: Scripts
    :apple: - This sucks part 17

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo "Copying SFML frameworks to /Library/Frameworks"
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /Library/Frameworks
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /Library/Frameworks
-
-echo "Library path environment variable before:"
-echo $LD_LIBRARY_PATH
-
-echo "Setting Library path environment variable"
-export LD_LIBRARY_PATH=/Library/Frameworks
-
-echo "Library path environment variable after:"
-echo $LD_LIBRARY_PATH
+# echo "Copying SFML frameworks to /Library/Frameworks"
+# sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /Library/Frameworks
+# sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /Library/Frameworks
+#
+# echo "Library path environment variable before:"
+# echo $LD_LIBRARY_PATH
+# 
+# echo "Setting Library path environment variable"
+# export LD_LIBRARY_PATH=/Library/Frameworks
+# 
+# echo "Library path environment variable after:"
+# echo $LD_LIBRARY_PATH
 
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5_mac
@@ -21,6 +21,5 @@ make --debug=b config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
-find /Library/Frameworks -type d
 exit 1
 fi
