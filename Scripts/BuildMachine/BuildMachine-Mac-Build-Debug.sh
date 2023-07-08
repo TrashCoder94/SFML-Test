d132f414ce7d4b51043f8354dcf5010607aee8a5
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-08 15:32:35 +0100

    Update BuildMachine-Mac-Build-Debug.sh
    :apple: - This sucks part 6

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -8,7 +8,7 @@ echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5_mac
 ThirdParty/premake/Binaries/premake5_mac --file=premake5.lua gmake2
 
-make config=debug_x86_64
+make config=debug_x86_64 -v
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
