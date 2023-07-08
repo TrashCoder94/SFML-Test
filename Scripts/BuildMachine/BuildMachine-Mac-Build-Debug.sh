f79a9c8cec8d70b178fd86d8e4a0dd36b7f076c1
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-08 13:07:21 +0100

    Update: Scripts
    :apple: - FLAC/OpenAL frameworks are provided with the Mac binaries, no need to install them via brew as well. Saw that this was a Frameworks folder on the build machine potentially? So trying to copy there!

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 echo "Copying SFML frameworks to /Library/Frameworks/ folder"
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /Library/Frameworks
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /Library/Frameworks
-
-echo "Copying SFML frameworks to /users/runner/Library/Frameworks/ folder"
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /users/runner/Library/Frameworks
-cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /users/runner/Library/Frameworks
+cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks /usr/local/Frameworks
+cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs /usr/local/Frameworks
 
 echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5_mac
@@ -17,9 +13,5 @@ if [ $? -ne 0 ]
 then
 echo "Building debug configuration failed, exiting out..."
 find /usr/local/Cellar/sfml -type d
-realpath Window.hpp
-realpath FLAC.framework
-realpath sfml-audio.framework
-realpath sfml-audio.2.5.1.framework
 exit 1
 fi
