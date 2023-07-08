512250464e2335fa783fb0ece023fc14ee73aba7
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-08 22:02:16 +0100

    Update: Scripts
    :apple: - This sucks part 28

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -12,15 +12,5 @@ make verbose=1 config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building debug configuration failed, exiting out..."
-find /Library/Frameworks/OpenAL.framework -type d
-find /Library/Frameworks/sfml-audio.framework -type d
-find /Library/Frameworks/FLAC.framework -type d
-find /Library/Frameworks/flac.framework -type d
-find ~/Library/Frameworks/FLAC.framework -type d
-find ~/Library/Frameworks/flac.framework -type d
-find /System/Library/Frameworks/FLAC.framework -type d
-find /System/Library/Frameworks/flac.framework -type d
-find ~/System/Library/Frameworks/FLAC.framework -type d
-find ~/System/Library/Frameworks/flac.framework -type d
 exit 1
 fi
