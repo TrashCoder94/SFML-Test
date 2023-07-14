30350d01c606726dd681ef9f78311b3244944873
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-14 18:06:40 +0100

    Update premake5.lua
    :apple: - $(PWD) and pwd work differently... pwd will print the current working directory for terminal use. $(PWD) will use the project working directory (e.g. the root of the repository)

--- premake5.lua
+++ premake5.lua
@@ -170,9 +170,9 @@ function includeAndLinkSFML()
 		postbuildcommands
 		{
 			"pwd",
-			"sudo mkdir $(PWD)/../%{cfg.targetdir}/Frameworks",
-			"sudo cp -R $(PWD)/../ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ $(PWD)/../%{cfg.targetdir}/Frameworks/",
-			"sudo cp -R $(PWD)/../ThirdParty/SFML-Binaries-Test/Mac/extlibs/ $(PWD)/../%{cfg.targetdir}/Frameworks/"
+			"sudo mkdir $(PWD)/%{cfg.targetdir}/Frameworks",
+			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ $(PWD)/%{cfg.targetdir}/Frameworks/",
+			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ $(PWD)/%{cfg.targetdir}/Frameworks/"
 		}
 	filter ""
 	
