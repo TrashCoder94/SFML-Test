91dbbd64c4d903ecc42034b67e72d70cd87ed00e
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-14 19:30:33 +0100

    Update premake5.lua
    :apple: - This sucks part 40

--- premake5.lua
+++ premake5.lua
@@ -162,11 +162,11 @@ function includeAndLinkSFML()
 		-- Using /Library/Frameworks since that seems to be the standard place for all non system frameworks...
 		buildoptions 
 		{
-			"-F /Library/Frameworks"
+			"-F /Library/Frameworks %{wks.location}/ThirdParty/SFML-Binaries-Test/Mac/extlibs %{wks.location}/ThirdParty/SFML-Binaries-Test/Mac/Frameworks"
 		}
 		linkoptions 
 		{
-			"-F /Library/Frameworks"
+			"-F /Library/Frameworks  %{wks.location}/ThirdParty/SFML-Binaries-Test/Mac/extlibs %{wks.location}/ThirdParty/SFML-Binaries-Test/Mac/Frameworks"
 		}
 		postbuildcommands
 		{
