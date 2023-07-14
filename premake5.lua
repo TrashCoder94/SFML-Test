e9db796a117210f2f4a3842da722697846146eac
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-14 18:33:52 +0100

    Update: Scripts
    :apple: - This sucks part 36

--- premake5.lua
+++ premake5.lua
@@ -170,9 +170,9 @@ function includeAndLinkSFML()
 		postbuildcommands
 		{
 			"pwd",
-			"sudo mkdir -p ../%{cfg.targetdir}/Frameworks",
-			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ ../%{cfg.targetdir}/Frameworks/",
-			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ ../%{cfg.targetdir}/Frameworks/"
+			"sudo mkdir -p /%{cfg.targetdir}/Frameworks",
+			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ /%{cfg.targetdir}/Frameworks/",
+			"sudo cp -R $(PWD)/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ /%{cfg.targetdir}/Frameworks/"
 		}
 	filter ""
 	
