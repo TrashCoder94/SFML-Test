a827436976eba957591cad3b72f2bfa501f63fe7
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-14 17:01:36 +0100

    Revert "Update: Frameworks"
    This reverts commit 9606a61dfe60bc7f4f9ecdd28269e83568110e57.

--- premake5.lua
+++ premake5.lua
@@ -167,11 +167,6 @@ function includeAndLinkSFML()
 		{
 			"-F /Library/Frameworks"
 		}
-		postbuildcommands
-		{
-			"sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ /$(CURDIR)/%{cfg.targetdir}/Frameworks/",
-			"sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs/ /$(CURDIR)/%{cfg.targetdir}/Frameworks/"
-		}
 	filter ""
 	
 	filter "system:linux"
