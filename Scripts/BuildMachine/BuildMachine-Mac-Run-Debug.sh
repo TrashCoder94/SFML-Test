57820c458cb4dc6372c8becb381b602a935eda5f
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-14 15:53:51 +0100

    Update: Scripts
    :apple: - Fixing test. Trying the copy framework commands through the CI script instead.

--- Scripts/BuildMachine/BuildMachine-Mac-Run-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Run-Debug.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Copying SFML frameworks to /Library/Frameworks"
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ /Library/Frameworks/
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs/ /Library/Frameworks/
-
 echo "Running Debug HelloWorld executable!"
 chmod +x Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
 Binaries/Debug-macosx-x86_64/HelloWorld/HelloWorld
--- Scripts/BuildMachine/BuildMachine-Mac-Run-Release.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Run-Release.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Copying SFML frameworks to /Library/Frameworks"
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/Frameworks/ /Library/Frameworks/
-sudo cp -R ./ThirdParty/SFML-Binaries-Test/Mac/extlibs/ /Library/Frameworks/
-
 echo "Running Release HelloWorld executable!"
 chmod +x Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
 Binaries/Release-macosx-x86_64/HelloWorld/HelloWorld
