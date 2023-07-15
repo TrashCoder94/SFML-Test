e7faceab2383cabb3583a71464877ff1cdf24935
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-15 12:10:00 +0100

    Update premake5.lua
    :apple: - Trying without the slash at the front. Think I remember reading somewhere that the slash at the front represents an absolute filepath. Just using dot to hint that it's relative

--- premake5.lua
+++ premake5.lua
@@ -165,8 +165,8 @@ function includeAndLinkSFML()
 		{
 			--"pwd",
 			--"sudo mkdir -p /%{cfg.targetdir}/Frameworks",
-			"find /%{cfg.targetdir} -type f",
-			"sudo cp -R /usr/local/Cellar/sfml/2.6.0/lib /%{cfg.targetdir}/"
+			"find %{cfg.targetdir} -type f",
+			"sudo cp -R /usr/local/Cellar/sfml/2.6.0/lib %{cfg.targetdir}/"
 		}
 	filter ""
 	
