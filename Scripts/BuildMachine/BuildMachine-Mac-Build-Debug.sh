3f8a89132a93d576767d2be9fc3c733a7780c38d
TrashCoder94 <jmottershead1994@gmail.com>
2023-07-16 12:28:14 +0100

    Revert "Update BuildMachine-Mac-Build-Debug.sh"
    This reverts commit ab18d364adc36c23efec38392f3047fb2bfb8e3e.

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -8,39 +8,39 @@ pwd
 echo "Creating extlibs Symbolic Links"
 
 # FLAC
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/FLAC" "Versions/A/FLAC"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Versions/A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/FLAC" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Versions/A/FLAC"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/FLAC.framework/Versions/A/Resources"
 
 # Freetype
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/freetype" "Versions/A/freetype"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/freetype" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Versions/A/freetype"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/freetype.framework/Versions/A"
 
 # Ogg
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/ogg" "Versions/A/ogg"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/ogg" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Versions/A/ogg"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/ogg.framework/Versions/A"
 
 # OpenAL
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/OpenAL" "Versions/A/OpenAL"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/OpenAL" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Versions/A/OpenAL"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/OpenAL.framework/Versions/A"
 
 # Vorbis
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/vorbis" "Versions/A/vorbis"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/vorbis" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Versions/A/vorbis"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbis.framework/Versions/A"
 
 # Vorbisenc
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/vorbisenc" "Versions/A/vorbisenc"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/vorbisenc" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Versions/A/vorbisenc"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisenc.framework/Versions/A"
 
 # Vorbisfile
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/vorbisfile" "Versions/A/vorbisfile"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Resources" "Versions/A/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Versions/Current" "A"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/vorbisfile" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Versions/A/vorbisfile"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Versions/A/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/extlibs/vorbisfile.framework/Versions/A"
 
 # ==================================
 # EXTLIBS
@@ -48,35 +48,35 @@ sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-T
 echo "Creating SFML symbolic links"
 
 # Main
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/SFML" "Versions/2.5.1/SFML"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Headers" "Versions/2.5.1/Headers"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/SFML" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/2.5.1/SFML"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Headers" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/2.5.1/Headers"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/SFML.framework/Versions/2.5.1"
 
 # Audio
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/sfml-audio" "Versions/2.5.1/sfml-audio"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/sfml-audio" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Versions/2.5.1/sfml-audio"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-audio.framework/Versions/2.5.1"
 
 # Graphics
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/sfml-graphics" "Versions/2.5.1/sfml-graphics"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/sfml-graphics" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Versions/2.5.1/sfml-graphics"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-graphics.framework/Versions/2.5.1"
 
 # Network
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/sfml-network" "Versions/2.5.1/sfml-network"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/sfml-network" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Versions/2.5.1/sfml-network"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-network.framework/Versions/2.5.1"
 
 # System
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/sfml-system" "Versions/2.5.1/sfml-system"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/sfml-system" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Versions/2.5.1/sfml-system"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-system.framework/Versions/2.5.1"
 
 # Window
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/sfml-window" "Versions/2.5.1/sfml-window"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Resources" "Versions/2.5.1/Resources"
-sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Versions/Current" "2.5.1"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/sfml-window" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Versions/2.5.1/sfml-window"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Resources" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Versions/2.5.1/Resources"
+sudo ln –sf "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Versions/Current" "/Users/runner/work/SFML-Test/SFML-Test/ThirdParty/SFML-Binaries-Test/Mac/Frameworks/sfml-window.framework/Versions/2.5.1"
 
 echo "Finished creating symbolic links"
 
